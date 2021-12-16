fd880d8824c9bdb9d7983b7e1d64e42b27d55c61
Eric Butz <ericgbutz@gmail.com>
2021-12-15 19:19:56 -0700

    Add xfer

--- properties.js
+++ properties.js
@@ -1,15 +1,15 @@
 const properties = {
   server: 'https://fiotestnet.greymass.com',
-  publicKey: 'FIO7DaqRKziahVEALTZYyp2sAHf1wsCH4eqdAsUGh7y8ZMByG4feD',
-  privateKey: '5JfqKNmKpRBJZgzGZY2UuWjEEDEERohAiDeNQudty1SGdEFoUKn',
-  account: 'mzak3qnpwtai',
+  publicKey: '',
+  privateKey: '',
+  account: '',
   maxFee: 100000000000,
 
   action: "voteproducer",   // The action you want to execute (trnsfiopubky or voteproducer)
 
   // If using for trnsfiopubky
   transferAmount: 1000000000,  // Transfer amount in sufs
-  payeePubKey: "FIO6RkzYHuxaFt7YbyAGqEFFeFCoy2PJD1tZjzymghJk2z9gH2LVY",   // FIO Public Key of Payee
+  payeePubKey: "",   // FIO Public Key of Payee
 
   // If using for voteproducer
   voterFioAddress: "alice@purse",
